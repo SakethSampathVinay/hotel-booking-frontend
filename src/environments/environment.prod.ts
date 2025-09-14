6eb769749ef7b85d2d55c4049488bf4231c60b7e
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-09-14 17:16:57 +0530

    changed prod backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://hotel-booking-backend-74ai.onrender.com',
+  backendUrl: 'https://hotel-booking-node-backend.onrender.com',
 };
+// https://hotel-booking-backend-74ai.onrender.com
